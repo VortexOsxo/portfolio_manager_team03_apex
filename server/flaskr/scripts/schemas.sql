cad61282eb1807d8b1c85bda17c626487ed81be1
Leonardo Borges <leonardo.one@outlook.com>
2026-07-22 18:03:48 -0300

    Updated Database

--- server/flaskr/scripts/schemas.sql
+++ server/flaskr/scripts/schemas.sql
@@ -1,8 +1,13 @@
 CREATE DATABASE IF NOT EXISTS portfolio_db;
+   USE portfolio_db;
 
-USE portfolio_db;
-
-CREATE TABLE IF NOT EXISTS stocks (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    symbol VARCHAR(10) NOT NULL UNIQUE
-);
+CREATE TABLE `holdings` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `stock_ticker` varchar(10) NOT NULL,
+  `company_name` varchar(100) DEFAULT NULL,
+  `shares` decimal(15,4) NOT NULL,
+  `cost_basis` decimal(15,2) NOT NULL,
+  `current_price` decimal(15,2) DEFAULT NULL,
+  `purchase_date` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
