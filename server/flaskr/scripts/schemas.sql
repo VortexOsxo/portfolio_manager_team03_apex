eb8e9fe286a0ed7fe93bc2a66ed8993239ed5c03
Philippe Martin <philippeoliviermartin@gmail.com>
2026-07-23 16:35:57 -0400

    separated db into two table, holdings and stocks

--- server/flaskr/scripts/schemas.sql
+++ server/flaskr/scripts/schemas.sql
@@ -1,13 +1,21 @@
-CREATE DATABASE IF NOT EXISTS portfolio_db;
-   USE portfolio_db;
+DROP DATABASE IF EXISTS portfolio_db;
+CREATE DATABASE portfolio_db;
+
+USE portfolio_db;
+
+CREATE TABLE `stocks` (
+  `stock_id` int NOT NULL AUTO_INCREMENT,
+  `ticker` varchar(10) UNIQUE NOT NULL,
+  `company_name` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`stock_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 CREATE TABLE `holdings` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `stock_ticker` varchar(10) NOT NULL,
-  `company_name` varchar(100) DEFAULT NULL,
+  `holding_id` int NOT NULL AUTO_INCREMENT,
+  `stock_id` int NOT NULL,
   `shares` decimal(15,4) NOT NULL,
   `cost_basis` decimal(15,2) NOT NULL,
-  `current_price` decimal(15,2) DEFAULT NULL,
   `purchase_date` date NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`holding_id`),
+  FOREIGN KEY (`stock_id`) REFERENCES `stocks`(`stock_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
